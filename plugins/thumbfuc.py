89c798b931aaeb39a9bf6d2fa0acf194056a73a0
@CINEMAPRANTHANZZ1 <103062877+Aradhyaph@users.noreply.github.com>
2022-06-07 20:38:05 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -9,7 +9,7 @@ async def viewthumb(client,message):
 	   chat_id=message.chat.id, 
 	   photo=thumb)
     else:
-        await message.reply_text("**You dont have any custom Thumbnail**") 
+        await message.reply_text("**വെളച്ചിൽ എടുക്കരുത് കേട്ടോ**") 
 		
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
@@ -20,5 +20,5 @@ async def removethumb(client,message):
 async def addthumbs(client,message):
     file_id = str(message.photo.file_id)
     addthumb(message.chat.id , file_id)
-    await message.reply_text("**Your Thumbnail Saved** ✅")
+    await message.reply_text("**Thumbnail Saved** ")
 	
